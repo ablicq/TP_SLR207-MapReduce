a302feece73bf01eacb3c556c0145ca779afb120
Aurelien Blicq <blicqaurelien@gmail.com>
2019-02-16 16:50:58 +0100

    modify main to just print the head of the HashMap

--- hadoop/src/Main.java
+++ hadoop/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,7 +8,14 @@ public class Main {
         HashMap<String, Integer> wordCount = WordCounter.count(args[0]);
         long endTime = System.currentTimeMillis();
         assert wordCount != null;
-        wordCount.forEach((key, value) -> System.out.println(key + " " + value));
+        // print first 50 elements (all the elements if there are less than 50)
+        Iterator<Map.Entry<String, Integer>> it = wordCount.entrySet().iterator();
+        int cpt = 0;
+        while(cpt < 50 && it.hasNext()){
+            Map.Entry<String, Integer> entry = it.next();
+            System.out.println(entry.getKey() + " " + entry.getValue());
+            cpt++;
+        }
         long computationTime = endTime - startTime;
         System.out.println("\nComputation time: " + computationTime + "ms");
     }
