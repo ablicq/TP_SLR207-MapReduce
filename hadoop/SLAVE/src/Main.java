b7c1fd575f551391cd635277faf03294a2864216
Aurelien Blicq <blicqaurelien@gmail.com>
2019-02-26 10:41:51 +0100

    prepare the slave for timeout test

--- hadoop/SLAVE/src/Main.java
+++ hadoop/SLAVE/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int result = 3+2;
+        try {Thread.sleep(10000);} catch (Exception e){e.printStackTrace();}
         System.out.println(result);
     }
 }
