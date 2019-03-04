3fea21275f779cd7e2bc4a318f6e476493bab38a
Aurelien Blicq <blicqaurelien@gmail.com>
2019-03-04 15:56:27 +0100

    deployer done

--- hadoop/Deploy/src/Deployer.java
+++ hadoop/Deploy/src/Deployer.java
@@ -4,28 +4,32 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Deployer {
-    private String configFile;
+    ArrayList<String> hostsList = new ArrayList<>();
 
     public Deployer(String configFile) {
-        this.configFile = configFile;
+        parseHosts(configFile);
     }
 
-    public void runTest(){
+    private void parseHosts(String configFile){
         // read the hosts list from the config file
-        ArrayList<String> hostsList = new ArrayList<>();
         try (Scanner in = new Scanner(new FileInputStream(configFile))) {
             while (in.hasNextLine()){
                 String s = in.nextLine();
                 hostsList.add(s.split("\\s")[0]);
             }
         } catch (Exception e) { e.printStackTrace();}
+    }
 
+
+    public void runTest(){
         for(String host : hostsList){
-            ProcessBuilder pb = new ProcessBuilder("ssh",
+            ProcessBuilder pb = new ProcessBuilder(
+                    "ssh",
                     "-o", "UserKnownHostsFile=/dev/null",
                     "-o", "StrictHostKeyChecking=no",
                     "ablicq@"+host,
                     "hostname");
+
             pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
             pb.redirectError(ProcessBuilder.Redirect.INHERIT);
 
@@ -37,9 +41,42 @@ public class Deployer {
         }
     }
 
+    public void deploy(String jarFile){
+        for (String host : hostsList){
+            ProcessBuilder pb1 = new ProcessBuilder(
+                    "ssh",
+                    "-o", "UserKnownHostsFile=/dev/null",
+                    "-o", "StrictHostKeyChecking=no",
+                    "ablicq@"+host,
+                    "mkdir", "-p", "/tmp/ablicq",
+                    "&&",
+                    "hostname"
+            );
+
+            ProcessBuilder pb2 = new ProcessBuilder(
+                    "scp", jarFile, "ablicq@"+host+":/tmp/ablicq/slave.jar"
+            );
+
+            pb1.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+            pb1.redirectError(ProcessBuilder.Redirect.INHERIT);
+
+            pb2.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+            pb2.redirectError(ProcessBuilder.Redirect.INHERIT);
+
+            try {
+                Process p1 = pb1.start();
+                p1.waitFor();
+                pb2.start();
+
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        Deployer test = new Deployer("config.txt");
-        test.runTest();
+        Deployer deployer = new Deployer("config.txt");
+        deployer.runTest();
+        deployer.deploy("/tmp/ablicq/SLAVE.jar");
     }
 }
