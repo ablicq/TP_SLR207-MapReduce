8b20fa8706cd6a82c9ace1fed073bd89e039be6b
Aurelien Blicq <blicqaurelien@gmail.com>
2019-03-04 14:16:32 +0100

    timeout system finised

--- hadoop/MASTER/src/Main.java
+++ hadoop/MASTER/src/Main.java
@@ -1,10 +1,35 @@
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        long maxTimeMillis = 5000;
         ProcessBuilder pb = new ProcessBuilder("java", "-jar", "/tmp/ablicq/SLAVE.jar");
-        pb.redirectError(ProcessBuilder.Redirect.INHERIT);
-        pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-        pb.start();
+        Process p = pb.start();
+
+        BufferedInputStream pStd = new BufferedInputStream(p.getInputStream());
+        LinkedBlockingQueue<String> stdTimeOutQueue = new LinkedBlockingQueue<>();
+        ReadThread readStd = new ReadThread(pStd, stdTimeOutQueue);
+        readStd.start();
+
+        try {
+            String val = stdTimeOutQueue.poll(maxTimeMillis, TimeUnit.MILLISECONDS);
+            if(val == null){
+                throw new NullPointerException();
+            }
+            System.out.println(val);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            readStd.stopRun();
+        }
     }
 }
---
+++ hadoop/MASTER/src/ReadThread.java
@@ -0,0 +1,57 @@
+import java.io.BufferedInputStream;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * read the input from the given input stream and relays the data to the given queue
+ */
+public class ReadThread extends Thread {
+
+    /**
+     * the InputStream from which to read data
+     */
+    private BufferedInputStream in;
+
+    /**
+     * the queue on which to put data
+     */
+    private LinkedBlockingQueue<String> queue;
+
+    /**
+     * a boolean to stop the thread when wanted
+     */
+    private boolean isRunning = true;
+
+    /**
+     * the constructor of the thread
+     * @param in the InputStream from which to read data
+     * @param queue the queue on which to put data
+     */
+    public ReadThread(BufferedInputStream in, LinkedBlockingQueue<String> queue) {
+        this.in = in;
+        this.queue = queue;
+    }
+
+    /**
+     * read the input from the given input stream and relays the data to the given queue
+     */
+    @Override
+    public void run() {
+        while (isRunning) {
+            try {
+                if (in.available() > 0) {
+                    char read = (char) in.read();
+                    queue.put(Character.toString(read));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * sets the isRunning variable to false to stop the thread
+     */
+    public synchronized void stopRun(){
+        isRunning = false;
+    }
+}
