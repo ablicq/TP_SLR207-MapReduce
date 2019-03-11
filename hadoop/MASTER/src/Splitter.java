4dac7fa75cd86bc4c5e9329bdd15dd05e771bbe3
Aurelien Blicq <blicqaurelien@gmail.com>
2019-03-11 15:08:13 +0100

    debug splitter

--- hadoop/MASTER/src/Splitter.java
+++ hadoop/MASTER/src/Splitter.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ public class Splitter {
 
     public void deploy(){
         hosts.parallelStream().forEach((host) -> {
+
             ProcessBuilder mkdirPB = new ProcessBuilder(
                     "ssh",
                     "-o", "UserKnownHostsFile=/dev/null",
@@ -20,8 +22,18 @@ public class Splitter {
                     "mkdir", "-p", "/tmp/ablicq/splits"
             );
 
+            // get the list of files in /tmp/ablicq/splits to be able to copy these with scp
+            File splitsFolder = new File("/tmp/ablicq/splits");
+            File[] splitFiles = splitsFolder.listFiles();
+            ArrayList<String> splits = new ArrayList<>();
+            Arrays.stream(splitFiles).forEach(file -> splits.add("/tmp/ablicq/splits/"+file.getName()));
+
+            ArrayList<String> copyCmd = new ArrayList<>();
+            copyCmd.add("scp");
+            copyCmd.addAll(splits);
+            copyCmd.add("ablicq@"+host+":/tmp/ablicq/splits/");
             ProcessBuilder scpPB = new ProcessBuilder(
-                    "scp", "/tmp/ablicq/splits/*", "ablicq@"+host+":/tmp/ablicq/splits/"
+                    copyCmd
             );
 
             mkdirPB.redirectOutput(ProcessBuilder.Redirect.INHERIT);
