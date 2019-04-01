a7481afdb04ac3e8b123daac610cd09f3d50ec9f
Aurelien Blicq <blicqaurelien@gmail.com>
2019-04-01 13:55:41 +0200

    start reducer implementation in slave

--- hadoop/SLAVE/src/Main.java
+++ hadoop/SLAVE/src/Main.java
@@ -1,20 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // check the validity of the args format
-        if(args.length != 2){
-            System.err.println("Error: Wrong Arguments");
-            System.err.println("Usage: java -jar slave.jar <mode> <inputFile>");
-            System.exit(-1);
-        }
+//        // check the validity of the args format
+//        if(args.length != 2){
+//            System.err.println("Error: Wrong Arguments");
+//            System.err.println("Usage: java -jar slave.jar <mode> <inputFile>");
+//            System.exit(-1);
+//        }
 
         // extract information from args
         int mode = Integer.parseInt(args[0]);
-        String inFile = args[1];
 
         // apply the desired function to the input file
         switch (mode){
             case 0:
+                String inFile = args[1];
                 Mapper.map(inFile);
+                break;
+            case 1:
+                String key = args[1];
+                String outNo = args[2];
+                ArrayList<String> maps = new ArrayList<>(Arrays.asList(args).subList(2, args.length -1));
+                Reducer.reduce(key, outNo, maps);
+                break;
         }
     }
 }
---
+++ hadoop/SLAVE/src/Reducer.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Reducer {
+    public static void reduce(String key, String outNo, ArrayList<String> maps) {
+
+    }
+}
