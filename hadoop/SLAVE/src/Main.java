de932df3c15f359f83f3e75647de9ee1aa213956
Aurelien Blicq <blicqaurelien@gmail.com>
2019-04-01 14:52:57 +0200

    reduce phase done

--- hadoop/SLAVE/src/Main.java
+++ hadoop/SLAVE/src/Main.java
@@ -8,15 +8,20 @@ public class Main {
 
         // apply the desired function
         switch (mode){
-            case 0:
+            case 0: // splits to unsorted maps
                 String inFile = args[1];
                 Mapper.map(inFile);
                 break;
-            case 1:
-                String key = args[1];
+            case 1: // unsorted maps to sorted maps
+                String mapKey = args[1];
                 String outNo = args[2];
                 ArrayList<String> maps = new ArrayList<>(Arrays.asList(args).subList(2, args.length -1));
-                Mapper.sortMaps(key, outNo, maps);
+                Mapper.sortMaps(mapKey, outNo, maps);
+                break;
+            case 2: // sorted maps to reduced maps
+                String reduceKey = args[1];
+                String inNo = args[2];
+                Reducer.reduce(reduceKey, inNo);
                 break;
         }
     }
--- hadoop/SLAVE/src/Reducer.java
+++ hadoop/SLAVE/src/Reducer.java
@@ -1,3 +1,27 @@
-public class Reducer {
+import java.io.*;
 
+public class Reducer {
+    public static void reduce(String key, String inNo){
+        String inFile = "/tmp/ablicq/maps/SM" + inNo + ".txt";
+        String outFile = createOut(inNo);
+        int nbKey = 0;
+        try(BufferedReader inReader = new BufferedReader(new FileReader(inFile))) {
+            nbKey = inReader.lines().mapToInt(l -> Integer.parseInt(l.split("\\s")[1])).sum();
+        } catch (IOException e) {e.printStackTrace();}
+        try (PrintWriter outPrint = new PrintWriter(new FileWriter(outFile))) {
+            outPrint.println(key + " " + nbKey);
+        } catch (IOException e) {e.printStackTrace();}
+    }
+
+    private static String createOut(String inNo) {
+        String outFile = "/tmp/ablicq/reduces/RM"+inNo+".txt";
+        ProcessBuilder pb = new ProcessBuilder("mkdir", "-p", "/tmp/ablicq/reduces/");
+        try{
+            Process p = pb.start();
+            p.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return outFile;
+    }
 }
