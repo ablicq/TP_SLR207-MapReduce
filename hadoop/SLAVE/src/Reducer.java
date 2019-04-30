a22c1d3aaf599174a7c10c71a57e72900e0e2709
Aurelien Blicq <blicqaurelien@gmail.com>
2019-04-30 16:18:39 +0200

    refactor and comment the slave Reducer class

--- hadoop/SLAVE/src/Reducer.java
+++ hadoop/SLAVE/src/Reducer.java
@@ -1,27 +1,50 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Reducer {
+    /**
+     * Reduce the sorted maps by counting the occurrences of the key in the inFile and put it in the outFile
+     * @param key the key to reduce
+     * @param inNo the index of the key to reduce (to get the inFile and outFile paths)
+     */
     public static void reduce(String key, String inNo){
-        String inFile = "/tmp/ablicq/maps/SM" + inNo + ".txt";
-        String outFile = createOut(inNo);
-        int nbKey = 0;
-        try(BufferedReader inReader = new BufferedReader(new FileReader(inFile))) {
-            nbKey = inReader.lines().mapToInt(l -> Integer.parseInt(l.split("\\s")[1])).sum();
-        } catch (IOException e) {e.printStackTrace();}
-        try (PrintWriter outPrint = new PrintWriter(new FileWriter(outFile))) {
+        // create reduces folder if it doesn't exist
+        if (Files.notExists(Paths.get("/tmp/ablicq/reduces")))
+            createReducesFolder();
+
+        // get inFile, outFile, and perform the map
+        try(BufferedReader inReader = createIn(inNo);
+            PrintWriter outPrint = createOut(inNo))
+        {
+            // compute the sum of the second characters of the lines of the input file
+            int nbKey = inReader.lines()
+                    .mapToInt(l -> Integer.parseInt(l.split("\\s")[1]))
+                    .sum();
+            // format the result and put it in the output file
             outPrint.println(key + " " + nbKey);
-        } catch (IOException e) {e.printStackTrace();}
+        }
+        catch (IOException e) {e.printStackTrace();}
     }
 
-    private static String createOut(String inNo) {
-        String outFile = "/tmp/ablicq/reduces/RM"+inNo+".txt";
+
+    /**
+     * create the reduce folder in the directory /tmp/ablicq
+     */
+    private static void createReducesFolder() {
         ProcessBuilder pb = new ProcessBuilder("mkdir", "-p", "/tmp/ablicq/reduces/");
-        try{
-            Process p = pb.start();
-            p.waitFor();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-        return outFile;
+        try{ pb.start().waitFor(); }
+        catch (IOException | InterruptedException e) { e.printStackTrace(); }
+    }
+
+
+    private static BufferedReader createIn(String inNo) throws FileNotFoundException {
+        String inFile = "/tmp/ablicq/maps/SM" + inNo + ".txt";
+        return new BufferedReader(new FileReader(inFile));
+    }
+
+    private static PrintWriter createOut(String inNo) throws IOException {
+        String outFile = "/tmp/ablicq/reduces/RM"+inNo+".txt";
+        return new PrintWriter(new FileWriter(outFile));
     }
 }
