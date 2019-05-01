741a079f8d2572f290362a9f7ad3a79daa5c42df
Aurelien Blicq <blicqaurelien@gmail.com>
2019-05-01 17:55:42 +0200

    refactor master: change main

--- hadoop/MASTER/src/Main.java
+++ hadoop/MASTER/src/Main.java
@@ -1,5 +1,12 @@
+import map_reduce.JobsManager;
+
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-
+        JobsManager jobsManager = new JobsManager("../slaves.config", 3);
+        jobsManager.runAll();
+        HashMap<String, Integer> wordCount = jobsManager.fetchResults();
+        wordCount.forEach((word, count) -> System.out.println(word + ": " +count));
     }
 }
