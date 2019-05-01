a483d44ec193631228c5b82bf6a762c2af122230
Aurelien Blicq <blicqaurelien@gmail.com>
2019-05-01 12:05:49 +0200

    refactor master: add Mapper class

--- hadoop/MASTER/src/JobsManager.java
+++ hadoop/MASTER/src/JobsManager.java
@@ -7,14 +7,12 @@ import java.util.concurrent.TimeUnit;
 
 public class JobsManager {
     private ArrayList<String> hosts = new ArrayList<>();
-    private HashMap<String, ArrayList<Integer>> splitAssignments = new HashMap<>();
     private String splitsLoc = "/tmp/ablicq/splits";
     private String mapsLoc = "/tmp/ablicq/maps";
     private String reducesLoc = "/tmp/ablicq/reduces";
-    private Integer nbSplits = 3;
 
-    private HashMap<String, ArrayList<Integer>> keySplitMap = new HashMap<>();
-    private HashMap<Integer, String> splitHostMap = new HashMap<>();
+    private Mapper mapper;
+
     private HashMap<String, ArrayList<String>> mapAssignments = new HashMap<>();
     private HashMap<String, HashSet<Integer>> filesToTransfer = new HashMap<>();
 
@@ -24,9 +22,9 @@ public class JobsManager {
      * Assign slaves to the splits, deploySplits the splits to the slaves, and order the maps to run
      * @param hosts the hosts of the slave machines
      */
-    public JobsManager(ArrayList<String> hosts) {
+    public JobsManager(ArrayList<String> hosts, int nbSplits) {
         this.hosts = hosts;
-        assignSplits();
+        this.mapper = new Mapper(hosts, nbSplits);
     }
 
 
@@ -34,15 +32,11 @@ public class JobsManager {
      * Assign slaves to the splits, deploy the splits to the slaves, and order the maps to run
      * @param configFile a path to a file containing the hostnames of the slaves
      */
-    public JobsManager(String configFile){
+    public JobsManager(String configFile, int nbSplits){
         parseHosts(configFile);
-        assignSplits();
+        this.mapper = new Mapper(hosts, nbSplits);
     }
 
-
-    private String splitNoToLoc(Integer splitNo) {
-        return splitsLoc + "/S" + splitNo + ".txt";
-    }
 
     private String mapNoToLoc(Integer mapNo) {
         return mapsLoc + "/UM" + mapNo + ".txt";
@@ -71,123 +65,8 @@ public class JobsManager {
         } catch (Exception e) { e.printStackTrace();}
     }
 
-
-    /**
-     * Assign each split to a host that will have to process it for the map phase
-     */
-    private void assignSplits(){
-        int nbHosts = hosts.size();
-        for(int i = 0; i < nbSplits; ++i){
-            String h = hosts.get(i % nbHosts);
-            if(splitAssignments.containsKey(h)) {
-                splitAssignments.get(h).add(i);
-            } else {
-                splitAssignments.put(h, new ArrayList<>(Collections.singleton(i)));
-            }
-            splitHostMap.put(i, h);
-        }
-    }
-
-
-    /**
-     * Deploy the splits ie send each split to the slave that have been assigned to do the map with it
-     */
-    public void deploySplits(){
-        hosts.parallelStream().forEach((host) -> {
-
-            ProcessBuilder mkdirPB = new ProcessBuilder(
-                    "ssh",
-                    "-o", "UserKnownHostsFile=/dev/null",
-                    "-o", "StrictHostKeyChecking=no",
-                    "ablicq@"+host,
-                    "mkdir", "-p", "/tmp/ablicq/splits"
-            );
-
-
-            var splitLocs = new ArrayList<String>();
-            splitAssignments.get(host).forEach(i -> splitLocs.add(splitNoToLoc(i)));
-            ArrayList<String> copyCmd = new ArrayList<>();
-            copyCmd.add("scp");
-            copyCmd.addAll(splitLocs);
-            copyCmd.add("ablicq@"+host+":/tmp/ablicq/splits/");
-
-            ProcessBuilder scpPB = new ProcessBuilder(
-                    copyCmd
-            );
-
-            mkdirPB.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-            mkdirPB.redirectError(ProcessBuilder.Redirect.INHERIT);
-
-            scpPB.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-            scpPB.redirectError(ProcessBuilder.Redirect.INHERIT);
-
-            try {
-                Process mkdir = mkdirPB.start();
-                mkdir.waitFor();
-                scpPB.start();
-            } catch (IOException | InterruptedException e){
-                e.printStackTrace();
-            }
-        });
-    }
-
-
-    /**
-     * Tell each slave to do the map with the split it has been assigned to
-     */
-    public void runMaps(){
-        ArrayList<LinkedBlockingQueue<String>> queues = new ArrayList<>();
-        hosts.parallelStream().forEach(host ->{
-            ArrayList<String> sshWrapper = new ArrayList<>(Arrays.asList("ssh",
-                    "-o", "UserKnownHostsFile=/dev/null",
-                    "-o", "StrictHostKeyChecking=no",
-                    "ablicq@"+host));
-            splitAssignments.get(host).forEach(split ->{
-                ArrayList<String> cmd = new ArrayList<>(sshWrapper);
-                cmd.addAll(Arrays.asList("java", "-jar", "/tmp/ablicq/slave.jar", "0", split.toString()));
-                ProcessBuilder runMapBuilder = new ProcessBuilder(cmd);
-                try{
-                    // Run the map
-                    Process runMapProcess = runMapBuilder.start();
-                    runMapProcess.waitFor();
-
-                    // prepare the output reading
-                    BufferedInputStream pStd = new BufferedInputStream(runMapProcess.getInputStream());
-                    LinkedBlockingQueue<String> stdTimeOutQueue = new LinkedBlockingQueue<>();
-                    ReadThread readStd = new ReadThread(pStd, stdTimeOutQueue);
-                    readStd.start();
-
-                    // read the output
-                    String val = "";
-                    String key = "";
-                    while(val != null)
-                    {
-                        if (val.equals("\n")) {
-                            if(keySplitMap.containsKey(key)) {
-                                keySplitMap.get(key).add(split);
-                            } else {
-                                keySplitMap.put(key, new ArrayList<>(Collections.singleton(split)));
-                            }
-                            key = "";
-                        } else {
-                            key += val;
-                        }
-                        //System.out.print(val);
-                        val = stdTimeOutQueue.poll(1000, TimeUnit.MILLISECONDS);
-                    }
-                    readStd.stopRun();
-                } catch (IOException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
-        });
-        System.out.println("=====================================");
-        System.out.println("Map phase finished");
-        System.out.println("key -> splits:");
-        keySplitMap.forEach((key, value) -> {System.out.print(key + " -> "); value.forEach(i -> System.out.print(i + " ")); System.out.print("\n");});
-        System.out.println("split -> host:");
-        splitHostMap.forEach((key, value) -> System.out.println(key + " -> " + value));
-        System.out.println("=====================================");
+    public void runAll(){
+        mapper.map();
     }
 
     //******************************************************************************************************************
@@ -380,6 +259,7 @@ public class JobsManager {
 
     public static void main(String[] args) {
         JobsManager jobsManager = new JobsManager(args[0]);
+        jobsManager.assignSplits();
         jobsManager.deploySplits();
         jobsManager.runMaps();
         jobsManager.shuffle();
---
+++ hadoop/MASTER/src/Mapper.java
@@ -0,0 +1,173 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class Mapper {
+    //*****************************************************************************************************************
+    //*****************************************************************************************************************
+    //                                                FIELDS
+    //*****************************************************************************************************************
+    //*****************************************************************************************************************
+    private ArrayList<String> hosts;
+    private int nbSplits;
+
+    private HashMap<String, ArrayList<Integer>> splitAssignments = new HashMap<>();
+    private HashMap<Integer, String> splitHostMap = new HashMap<>();
+    private HashMap<String, ArrayList<Integer>> keySplitMap = new HashMap<>();
+
+    //*****************************************************************************************************************
+    //*****************************************************************************************************************
+    //                                              CONSTRUCTOR
+    //*****************************************************************************************************************
+    //*****************************************************************************************************************
+    public Mapper(ArrayList<String> hosts, int nbSplits) {
+        this.hosts = hosts;
+        this.nbSplits = nbSplits;
+    }
+
+    //*****************************************************************************************************************
+    //*****************************************************************************************************************
+    //                                                GETTERS
+    //*****************************************************************************************************************
+    //*****************************************************************************************************************
+    public HashMap<Integer, String> getSplitHostMap() {
+        return splitHostMap;
+    }
+
+    public HashMap<String, ArrayList<Integer>> getKeySplitMap() {
+        return keySplitMap;
+    }
+
+    //*****************************************************************************************************************
+    //*****************************************************************************************************************
+    //                                                METHODS
+    //*****************************************************************************************************************
+    //*****************************************************************************************************************
+    public void map(){
+        this.assignSplits();
+        this.deploySplits();
+        this.runMaps();
+    }
+
+    /**
+     * Assign each split to a host that will have to process it for the map phase
+     */
+    private void assignSplits() {
+        int nbHosts = hosts.size();
+        for (int i = 0; i < nbSplits; ++i) {
+            String h = hosts.get(i % nbHosts);
+            if (splitAssignments.containsKey(h)) {
+                splitAssignments.get(h).add(i);
+            } else {
+                splitAssignments.put(h, new ArrayList<>(Collections.singleton(i)));
+            }
+            splitHostMap.put(i, h);
+        }
+    }
+
+    /**
+     * Deploy the splits ie send each split to the slave that have been assigned to do the map with it
+     */
+    private void deploySplits() {
+        hosts.parallelStream().forEach((host) -> {
+
+            ProcessBuilder mkdirPB = new ProcessBuilder(
+                    "ssh",
+                    "-o", "UserKnownHostsFile=/dev/null",
+                    "-o", "StrictHostKeyChecking=no",
+                    "ablicq@" + host,
+                    "mkdir", "-p", "/tmp/ablicq/splits"
+            );
+
+
+            var splitLocs = new ArrayList<String>();
+            splitAssignments.get(host).forEach(i -> splitLocs.add(splitNoToLoc(i)));
+            ArrayList<String> copyCmd = new ArrayList<>();
+            copyCmd.add("scp");
+            copyCmd.addAll(splitLocs);
+            copyCmd.add("ablicq@" + host + ":/tmp/ablicq/splits/");
+
+            ProcessBuilder scpPB = new ProcessBuilder(
+                    copyCmd
+            );
+
+            mkdirPB.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+            mkdirPB.redirectError(ProcessBuilder.Redirect.INHERIT);
+
+            scpPB.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+            scpPB.redirectError(ProcessBuilder.Redirect.INHERIT);
+
+            try {
+                mkdirPB.start().waitFor();
+                scpPB.start();
+            } catch (IOException | InterruptedException e) { e.printStackTrace(); }
+        });
+    }
+
+    /**
+     * Tell each slave to do the map with the split it has been assigned to
+     */
+    private void runMaps(){
+        hosts.parallelStream().forEach(host ->{
+            ArrayList<String> sshWrapper = new ArrayList<>(Arrays.asList("ssh",
+                    "-o", "UserKnownHostsFile=/dev/null",
+                    "-o", "StrictHostKeyChecking=no",
+                    "ablicq@"+host));
+            splitAssignments.get(host).forEach(split ->{
+                ArrayList<String> cmd = new ArrayList<>(sshWrapper);
+                cmd.addAll(Arrays.asList("java", "-jar", "/tmp/ablicq/slave.jar", "0", split.toString()));
+                ProcessBuilder runMapBuilder = new ProcessBuilder(cmd);
+                try{
+                    // Run the map
+                    Process runMapProcess = runMapBuilder.start();
+                    runMapProcess.waitFor();
+
+                    // prepare the output reading
+                    BufferedInputStream pStd = new BufferedInputStream(runMapProcess.getInputStream());
+                    LinkedBlockingQueue<String> stdTimeOutQueue = new LinkedBlockingQueue<>();
+                    ReadThread readStd = new ReadThread(pStd, stdTimeOutQueue);
+                    readStd.start();
+
+                    // read the output
+                    String val = "";
+                    String key = "";
+                    while(val != null)
+                    {
+                        if (val.equals("\n")) {
+                            if(keySplitMap.containsKey(key)) {
+                                keySplitMap.get(key).add(split);
+                            } else {
+                                keySplitMap.put(key, new ArrayList<>(Collections.singleton(split)));
+                            }
+                            key = "";
+                        } else {
+                            key += val;
+                        }
+                        //System.out.print(val);
+                        val = stdTimeOutQueue.poll(1000, TimeUnit.MILLISECONDS);
+                    }
+                    readStd.stopRun();
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        });
+        System.out.println("=====================================");
+        System.out.println("Map phase finished");
+        System.out.println("key -> splits:");
+        keySplitMap.forEach((key, value) -> {System.out.print(key + " -> "); value.forEach(i -> System.out.print(i + " ")); System.out.print("\n");});
+        System.out.println("split -> host:");
+        splitHostMap.forEach((key, value) -> System.out.println(key + " -> " + value));
+        System.out.println("=====================================");
+    }
+
+    private String splitNoToLoc(Integer splitNo) {
+        String splitsLoc = "/tmp/ablicq/splits";
+        return splitsLoc + "/S" + splitNo + ".txt";
+    }
+}
